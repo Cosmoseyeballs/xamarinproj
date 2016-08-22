b55ce9efb3bfb07d7980c3dc158eca04b4bcf14f
Admin <Admin>
2016-08-22 11:29:43 +0200

    Uge2Opgave class, abstract, sealed

---
+++ Uge2OpgaveOOP-ConsoleApplication/Uge2OpgaveOOP-ConsoleApplication/U2O/Expression.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2O
+{
+    public abstract class Expression
+    {
+       public abstract double Evaluate();
+    }
+
+    public sealed class ConstantExpression : Expression
+    {
+        private readonly double _value;
+
+        public ConstantExpression(double value)
+        {
+            _value = value;
+        }
+
+        public override double Evaluate()
+        {
+            return _value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+
+    public abstract class BinaryExpression : Expression
+    {
+        protected Expression left { get; set; }
+        protected Expression right { get; set; }
+        
+        public abstract string OperatorSymbol { get; }
+
+        public sealed override string ToString()
+        {
+             
+            return String.Format("{0} {1} {2}", left, OperatorSymbol, right);
+        }
+    }
+
+
+
+    class PlusExpression : BinaryExpression
+    {
+   
+
+        public PlusExpression(Expression Expression1, Expression Expression2)
+        {
+            left = Expression1;
+            right = Expression2;
+        }
+
+   
+        public override double Evaluate()
+        {
+            return left.Evaluate() + right.Evaluate();
+        }
+
+        public override string OperatorSymbol
+        {
+            get
+            {
+                return "+";
+            }
+        }
+    }
+
+    class MinusExpression : BinaryExpression
+    {
+
+        public MinusExpression(Expression Expression1, Expression Expression2)
+        {
+            left = Expression1;
+            right = Expression2;
+        }
+
+        public override double Evaluate()
+        {
+            return left.Evaluate() - right.Evaluate();
+        }
+
+        public override string OperatorSymbol
+        {
+            get
+            {
+                return "-";
+            }
+        }
+    }
+
+
+
+    class MultiplyExpression : BinaryExpression
+    {
+        public MultiplyExpression(Expression Expression1, Expression Expression2)
+        {
+            left = Expression1;
+            right = Expression2;
+        }
+        public override double Evaluate()
+        {
+            return left.Evaluate() * right.Evaluate();
+        }
+
+        public override string OperatorSymbol
+        {
+            get
+            {
+                return "*";
+            }
+        }
+    }
+
+
+    abstract class UnaryExpression : Expression
+    {
+        protected Expression expr;
+    }
+
+    class NegateExpression : UnaryExpression
+    {
+        public override double Evaluate()
+        {
+            return -expr.Evaluate();
+        }
+
+        public override string ToString()
+        {
+            return "-"+ expr.ToString();
+        }
+    }
+
+}
---
+++ Uge2OpgaveOOP-ConsoleApplication/Uge2OpgaveOOP-ConsoleApplication/U2O/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create your expression here
+            //Expression expr = new PlusExpression (new ConstantExpression(4));
+            // Example:
+            Expression expr = new PlusExpression(
+                new ConstantExpression(4),
+                new PlusExpression(
+                    new ConstantExpression(1),
+                    new ConstantExpression(2)));
+
+            // We print the textual representation
+            Console.Write(expr.ToString());
+
+            Console.Write(" = ");
+
+            // ... and then the result of evaluating
+            Console.WriteLine(expr.Evaluate());
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
