5b7d373430525fe60ad21e9d1db91687f93f5f2b
Admin <Admin>
2016-08-25 14:58:31 +0200

    dependency injection

---
+++ Xamarin.Course.LINQ/CertificateGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Course.LINQ
+{
+    class CertificateGenerator
+    {
+        private PrimeGenerator _PrimeGenerator = new PrimeGenerator();
+        void GeneratorCertificate()
+        {
+            _PrimeGenerator.GetNextPrime();
+        }
+    }
+}
---
+++ Xamarin.Course.LINQ/PrimeGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.Course.LINQ
+{
+    class PrimeGenerator
+    {
+        private int _KnownPrimes;
+        readonly List<int> KnownPrimes;
+
+        public int GetNextPrime()
+        {
+            return 1;
+        }
+    }
+
+    interface IPrimeGenerator
+    {
+
+    }
+}
